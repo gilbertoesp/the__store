9e617f77ba46197eceb2cca1053497989d39745f
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2023-10-19 13:07:18 -0700

    Fix: Routing components for a clean interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +32,12 @@ register();
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent},
+      {path: 'catalogo', component: CatalogoComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent],
